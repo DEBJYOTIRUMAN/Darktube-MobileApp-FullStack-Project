422d4237cdf57c524fd62fa33f27bc2b4a9f1c63
DEBJYOTIRUMAN <debjyotiruman@gmail.com>
2024-04-04 09:53:45 +0530

    Update Server

--- Backend/server.js
+++ Backend/server.js
@@ -9,7 +9,7 @@ import cors from 'cors';
 const APP_PORT = process.env.PORT || 5000;
 
 //Database Connection
-mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true});
+mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
@@ -19,12 +19,16 @@ db.once('open', () => {
 global.appRoot = path.resolve(__dirname);
 
 app.use(cors());
-app.use(express.urlencoded({ extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use('/api', routes);
 app.use('/uploads', express.static('uploads'));
 app.use('/music', express.static('music'));
 app.use('/movies', express.static('movies'));
+app.use("/", (req, res) => {
+    res.send(`
+    <h1>Welcome to Darkvilla App Server🚀</h1>`);
+});
 
 app.use(errorHandler);
-app.listen(APP_PORT, ()=> console.log(`Listening on port ${APP_PORT}.`));
+app.listen(APP_PORT, () => console.log(`Listening on port ${APP_PORT}.`));
--- README.md
+++ README.md
@@ -3,4 +3,4 @@
  1. Go to the App folder.
  2. Open the terminal and type "yarn" or "npm install" then press enter to install the node package.
  3. After finishing installation of the node package type "yarn start" or "npm start" then press enter to start the project on your device.
- 4. Wait up to 30 seconds because this can cause a response delay for the first request that comes in after a period of web service inactivity.
+ 4. Wait up to 60 seconds or more because this can cause a response delay for the first request that comes in after a period of web service inactivity.
